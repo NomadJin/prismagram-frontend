a1cd19557bb5eb6c3e83e466ba9b360411039ea6
NomadJin <jinnykim000@gmail.com>
2019-09-18 00:10:12 +0900

    impleamet apollo client

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { gql } from "apollo-boost";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Routes from "./Routes";
+import { useQuery } from "react-apollo-hooks";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <Routes isLoggedIn={false} />
-    </>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Routes isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
