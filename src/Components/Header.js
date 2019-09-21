5e876020a68df21cdaeb804c3a2ce4f322ed62c8
NomadJin <jinnykim000@gmail.com>
2019-09-21 14:25:03 +0900

    header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { gql } from "apollo-boost";
 import { Compass, Logo, HeartEmpty, User } from "./Icons";
 import Input from "./Input";
 import useInput from "../Hooks/useInput";
+import { useQuery } from "react-apollo-hooks";
 
 const Header = styled.header`
   ${props => props.theme.whiteBox};
@@ -62,8 +64,21 @@ const HeaderLink = styled(Link)`
   }
 `;
 
-export default () => {
+export const ME = gql`
+  {
+    me {
+      username
+    }
+  }
+`;
+
+export default withRouter(({ history }) => {
   const search = useInput("");
+  const { data } = useQuery(ME);
+  const onSearchSubmit = e => {
+    e.preventDefault();
+    history.push(`/search?term=${search.value}`);
+  };
   return (
     <Header>
       <HeaderWrapper>
@@ -73,7 +88,7 @@ export default () => {
           </Link>
         </HeaderColumn>
         <HeaderColumn>
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <SearchInput
               value={search.value}
               onChange={search.onChange}
@@ -88,11 +103,17 @@ export default () => {
           <HeaderLink to="/notifications">
             <HeartEmpty />
           </HeaderLink>
-          <HeaderLink to="/#">
-            <User />
-          </HeaderLink>
+          {!(data && data.me) ? (
+            <HeaderLink to="/#">
+              <User />
+            </HeaderLink>
+          ) : (
+            <HeaderLink to={data.me.username}>
+              <User />
+            </HeaderLink>
+          )}
         </HeaderColumn>
       </HeaderWrapper>
     </Header>
   );
-};
+});
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,22 +3,27 @@ import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
+import Explore from "../Routes/Explore";
+import Search from "../Routes/Search";
+import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => (
-  <>
-    <Route exact path="/" component={Feed}></Route>
-  </>
+  <Switch>
+    <Route exact path="/" component={Feed} />
+    <Route exact path="/explore" component={Explore} />
+    <Route exact path="/search" component={Search} />
+    <Route exact path="/:username" component={Profile} />
+  </Switch>
 );
 
 const LoggedOutRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Auth} />
-  </>
+  </Switch>
 );
 
-const Routes = ({ isLoggedIn }) => (
-  <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
-);
+const Routes = ({ isLoggedIn }) =>
+  isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />;
 
 Routes.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired
